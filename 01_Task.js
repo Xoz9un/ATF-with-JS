6b798e838b7d6d05e6fba7fc15ecfad4f122c8f2
Vitaly Baranov <aleshapashkevicha@gmail.com>
2021-12-02 22:39:02 +0300

    Perform division of various types

--- 01_Task.js
+++ 01_Task.js
@@ -6,3 +6,7 @@ console.log('4' + !!0, 'name' + 4, 3 + !false)
 //(string * boolean, string * number, number * boolean)
 console.log('Js' * false, 'str' * 4, 5 * true)
 console.log('Js' * true, '4' * 4,5 * false )
+// Perform division of various types
+// (string/boolean, string/number, number/Boolean)
+console.log("str" / false, "str2" / 4,5 / false)
+console.log("str" / true, "10"/ 5, 5 / true)
--- 01_Task.js.bak
+++ 01_Task.js.bak
@@ -2,3 +2,7 @@
 //(string + boolean, string + number,number + boolean)
 console.log("4" + true, '3' + 4, 3 + !true )
 console.log('4' + !!0, 'name' + 4, 3 + !false)
+//Perform multiplication of various types
+//(string * boolean, string * number, number * boolean)
+console.log('Js' * false, 'str' * 4, 5 * true)
+console.log('Js' * true, '4' * 4,5 * false )
